e78c4dcd0e68ac2e0d00c5e98168e51e456cd198
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2024-11-01 08:50:43 +0000

    Updated packages, minor change to demo

--- examples/demo_ggremesh.jl
+++ examples/demo_ggremesh.jl
@@ -21,8 +21,8 @@ n = length(V) # Original number of points
 
 
 # Remeshing the surface 
-n1 = 10000 
-n2 = 5000
+n1 = 8000 
+n2 = 4000
 n3 = 1000
 F1,V1 = ggremesh(F,V; nb_pts=n1)
 F2,V2 = ggremesh(F,V; nb_pts=n2, remesh_anisotropy=0.0, remesh_gradation = 1.0, pre_max_hole_area=100, pre_max_hole_edges=0, post_max_hole_area=100, post_max_hole_edges=0, quiet=0, suppress = true)
@@ -36,12 +36,11 @@ fig = Figure(size=(1200,1200))
 
 ax1 = Axis3(fig[1, 1], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = "Original, $n points")
 hp1=poly!(ax1,GeometryBasics.Mesh(V,F),strokewidth=strokeWidth1,color=:white, shading = FastShading)
-# hp1 = mesh!(ax1,GeometryBasics.Mesh(V,F),color=:white, shading = FastShading)
 
 ax2 = Axis3(fig[1, 2], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = "Remeshed, $n1 points")
 hp2 = poly!(ax2,GeometryBasics.Mesh(V1,F1),strokewidth=strokeWidth1,color=:white, shading = FastShading)
 
-ax3 = Axis3(fig[2, 1], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = "Remeshed, $n2 points")
+ax3 = Axis3(fig[2, 1], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = "Remeshed, $n2 points, anisotropic")
 hp3 = poly!(ax3,GeometryBasics.Mesh(V2,F2),strokewidth=strokeWidth1,color=:white, shading = FastShading)
 
 ax4 = Axis3(fig[2, 2], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = "Remeshed, $n3 points")
